c6e330b5cfb877236842fb69456ea8f8114bd3df
SeungYup <seungyup26@gmail.com>
2022-01-28 15:41:14 +0900

    10869.cpp

---
+++ Cpp/10869.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    int A = 0, B = 0;
+    std::cin >> A >> B;
+
+    std::cout << A + B << std::endl;
+    std::cout << A - B << std::endl;
+    std::cout << A * B << std::endl;
+    std::cout << A / B << std::endl;
+    std::cout << A % B << std::endl;
+
+    return 0;
+}
