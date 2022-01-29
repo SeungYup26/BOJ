0b41c8c7c9cb21bb12f42923aa9a4966ed35ee28
SeungYup <seungyup26@gmail.com>
2022-01-29 19:16:55 +0900

    Solved 11022

---
+++ Cpp/Bronze/Bronze3/11022.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+    std::cin.tie(0);
+    std::cout.sync_with_stdio(false);
+
+    int T = 0;
+    std::cin >> T;
+
+    for (int i = 1; i <= T; i++)
+    {
+        int A = 0, B = 0;
+        std::cin >> A >> B;
+
+        std::cout << "Case #" << i << ": " << A << " + " << B << " = " << A + B << '\n';
+    }
+
+    return 0;
+}
