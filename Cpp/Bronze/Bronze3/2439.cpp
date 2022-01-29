e87bbae7dee23fc883d8a22fc66666111f49d6aa
SeungYup <seungyup26@gmail.com>
2022-01-29 19:30:13 +0900

    Solved 2439

---
+++ Cpp/Bronze/Bronze3/2439.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main()
+{
+    std::cin.tie(0);
+    std::cout.sync_with_stdio(false);
+
+    int N = 0;
+    std::cin >> N;
+
+    for (int i = N; i >= 1; i--)
+    {
+        for (int j = 0; j < i - 1; j++)
+            std::cout << ' ';
+        for (int j = N; j >= i; j--)
+            std::cout << '*';
+
+        std::cout << '\n';
+    }
+
+    return 0;
+}
