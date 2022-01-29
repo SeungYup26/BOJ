f5ca140a996016fe423065b29f5feb7a5a96e638
SeungYup <seungyup26@gmail.com>
2022-01-29 10:57:53 +0900

    Solved 14681

---
+++ Cpp/Bronze/Bronze4/14681.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    int x = 0, y = 0;
+    std::cin >> x >> y;
+
+    if (x > 0 && y > 0) std::cout << 1 << std::endl;
+    else if (x < 0 && y > 0) std::cout << 2 << std::endl;
+    else if (x < 0 && y < 0) std::cout << 3 << std::endl;
+    else if (x > 0 && y < 0) std::cout << 4 << std::endl;
+
+    return 0;
+}
