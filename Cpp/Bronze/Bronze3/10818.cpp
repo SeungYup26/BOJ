4bd967049f49c6436f95b9e7564d85faa3929979
SeungYup <seungyup26@gmail.com>
2022-02-04 11:29:41 +0900

    Solved 10818

---
+++ Cpp/Bronze/Bronze3/10818.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int main()
+{
+    std::cout.sync_with_stdio(false);
+    std::cin.tie(0);
+    
+    int N = 0;
+    std::cin >> N;
+
+    int  max     = -1000000;
+    int  min     = 1000000;
+    int* numbers = new int[N]{};
+
+    for (int i = 0; i < N; i++)
+    {
+        std::cin >> numbers[i];
+
+        if (numbers[i] < min)
+            min = numbers[i];
+        if (numbers[i] > max)
+            max = numbers[i];
+    }
+
+    std::cout << min << ' ' << max << '\n';
+
+    return 0;
+}
