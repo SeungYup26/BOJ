eedd6900b7caeebc7fdaeabea317183cdf2bee28
SeungYup <seungyup26@gmail.com>
2022-02-10 12:36:21 +0900

    Solved 1546.cpp

---
+++ Cpp/Bronze/Bronze1/1546.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+#include <algorithm>
+
+int main()
+{
+    int N = 0, M = 0;
+    std::cin >> N;
+
+    float* scores = new float[N]{};
+    for (int i = 0; i < N; i++)
+    {
+        std::cin >> scores[i];
+        if(scores[i] > M) M = scores[i];
+    }
+
+    float average = scores[0] / M * 100;
+    for (int i = 1; i < N; i++)
+    {
+        average += scores[i] / M * 100;
+    }
+
+    std::cout << average / N << '\n';
+
+    return 0;
+}
