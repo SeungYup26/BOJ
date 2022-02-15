520c1d274c8c12e6b91eddadeadc1638fe3d438f
SeungYup <seungyup26@gmail.com>
2022-02-15 21:03:46 +0900

    Solved 1181

---
+++ Cpp/Silver/Silver5/1181.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm>
+#include <set>
+
+struct Compare
+{
+    bool operator()(std::string_view a, std::string_view b) const
+    {
+        if (a.size() == b.size()) return a < b;
+        return a.size() < b.size();
+    }
+};
+
+int main()
+{
+    std::cout.sync_with_stdio(false);
+    std::cin.tie(0);
+
+    int N = 0;
+    std::cin >> N;
+
+    std::set<std::string, Compare> words;
+
+    for (int i = 0; i < N; i++)
+    {
+        std::string word;
+        std::cin >> word;
+        words.insert(word);
+    }
+
+    for (const auto& e : words)
+    {
+        std::cout << e << '\n';
+    }
+
+    return 0;
+}
