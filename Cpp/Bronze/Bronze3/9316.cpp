83a4b73e1ed027fb8b3332599c905a70decaedfe
SeungYup <seungyup26@gmail.com>
2022-02-21 09:50:14 +0900

    Solved 9316

---
+++ Cpp/Bronze/Bronze3/9316.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    int N = 0;
+    std::cin >> N;
+
+    for (int i = 1; i <= N; i++)
+    {
+        std::cout << "Hello World, Judge " << i << "!\n";
+    }
+
+    return 0;
+}
