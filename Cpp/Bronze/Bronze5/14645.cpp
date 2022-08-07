ce0fa96a03db191a02b9fe12650c9a512bd8f443
U2V1bmdZdXA= <seungyup26@gmail.com>
2022-08-07 19:48:02 +0900

    Solved 14645

---
+++ Cpp/Bronze/Bronze5/14645.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+    int n = 0, k = 0;
+    std::cin >> n >> k;
+
+    for (int i = 0; i < n; ++i)
+    {
+        int a = 0, b = 0;
+        std::cin >> a >> b;
+    }
+
+    std::cout << "비와이\n";
+
+    return 0;
+}
