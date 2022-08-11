a29fe4cc228ee93f173945ca35589c7c08b93f94
U2V1bmdZdXA= <seungyup26@gmail.com>
2022-08-11 23:02:59 +0900

    Solved 1065

---
+++ Cpp/Silver/Silver4/1065.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <array>
+
+bool continueNumber(int n)
+{
+    std::array<int, 3> digit { n % 10, n / 10 % 10, n / 100 };
+    if (digit[0] - digit[1] == digit[1] - digit[2])
+        return true;
+    return false;
+}
+
+int main()
+{
+    int n = 0;
+    std::cin >> n;
+    
+    if (n > 100) {
+        int result = 99;
+        for (int i = 100; i <= n; ++i)
+        {
+            if (continueNumber(i))
+                ++result;
+        }
+
+        std::cout << result << '\n';   
+    }
+    else
+        std::cout << n << '\n';
+
+    return 0;
+}
