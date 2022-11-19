1ce30f030de0145348eba993bd28d0bdead364a6
U2V1bmdZdXA= <seungyup26@gmail.com>
2022-11-19 10:31:27 +0900

    Edit code 11034

--- Cpp/Bronze/Bronze3/11034.cpp
+++ Cpp/Bronze/Bronze3/11034.cpp
@@ -3,10 +3,9 @@
 
 int main()
 {
-    while (true)
+    int a = 0, b = 0, c = 0;
+    while (std::cin >> a >> b >> c)
     {
-        int a = 0, b = 0, c = 0;
-        std::cin >> a >> b >> c;
         std::cout << std::max(b - a, c - b) - 1 << '\n';
     }
 
