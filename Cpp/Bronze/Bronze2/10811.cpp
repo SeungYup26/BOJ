eda0e0af26f8b9aba4878d06faca3f56ab010e93
U2V1bmdZdXA= <seungyup26@gmail.com>
2023-03-01 19:56:34 +0900

    Solved 10811

---
+++ Cpp/Bronze/Bronze2/10811.cpp
@@ -0,0 +1,31 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    int n{}, m{};
+    std::cin >> n >> m;
+
+    std::vector<int> basket(n);
+    for (int x{}; x < n; x++)
+    {
+        basket[x] = x + 1;
+    }
+
+    for (int x{}; x < m; x++)
+    {
+        int i{}, j{};
+        std::cin >> i >> j;
+        std::reverse(basket.begin() + i - 1, basket.begin() + j);
+    }
+
+    for (const auto& ball : basket)
+    {
+        std::cout << ball << ' ';
+    }
+
+    std::cout << '\n';
+
+    return 0;
+}
